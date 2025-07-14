e76bbd27bcad5b79dfa38f6e2a7cecf26a10afc0
Jimboni-tech <yunchengzhou@gmail.com>
2025-07-14 17:56:27 -0500

    deleted icon

--- vivaceapp/src/App.js
+++ vivaceapp/src/App.js
@@ -47,10 +47,7 @@ const App = () => {
             onClick={handleStartQuestClick}
             className="start-quest-button"
           >
-            {/* Inline SVG for Rocket icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon">
-              <path d="M4.5 17.5V19a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1.5M10.5 16.5H13.5M12 21v-3.5M12 17v-4M12 13V9M12 9V5M12 5V1M7 13h10M4 17h16" />
-            </svg>
+            
             Start Quest
           </button>
         ) : (
